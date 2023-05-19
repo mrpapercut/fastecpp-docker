0784ab27dc7650ad20732581a882a0cfa7af5e1e
Mischa Rodermond <m.rodermond@nfir.nl>
2023-05-19 13:08:19 +0200

    Adding support for ecpp-mpi

--- runandverify.sh
+++ runandverify.sh
@@ -4,7 +4,13 @@ export PYTHONPATH=/verify-cert/src
 CERT_NAME=$(echo $1 | md5sum - | awk '{print $1}')
 CERT_FILENAME=cert-$CERT_NAME
 
-ecpp -n "$1" -c -f /fastecpp/$CERT_FILENAME &> /dev/null
+RUN_MPI=0
+
+if [ $RUN_MPI != 0 ]; then
+    mpirun ecpp-mpi -n "$1" -c -f /fastecpp/$CERT_FILENAME
+else
+    ecpp -n "$1" -c -f /fastecpp/$CERT_FILENAME # &> /dev/null
+fi
 
 cp /fastecpp/$CERT_FILENAME.primo /verify-cert/certs/
 
