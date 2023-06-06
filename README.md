946cfe8232e67535b9fad9728818819e8dca71a2
mrpapercut <947626+mrpapercut@users.noreply.github.com>
2023-06-06 20:19:38 +0200

    Adding readme

---
+++ README.md
@@ -0,0 +1,30 @@
+# FastECPP in Docker
+
+This repo aims to run FastECPP checks in Docker.
+
+## Usage
+Run ./build.ps1 and ./run.ps1 to start the container
+
+While in the container terminal, use `runandverify.sh` if you just want to check a number:
+```bash
+# Single number
+./runandverify.sh "10^500+10^499+10^460+1"
+
+# From a file
+while IFS= read -r line; do ./runandverify.sh $line; done < testnumbers.txt
+```
+One of the generated certificates (.primo) can be found in `./certs`
+
+Run the following to perform only the ECPP check and get the certificates:
+```bash
+ecpp -n '10^500+10^499+10^460+1' -c -f /fastecpp/certs/cert-500-499-460
+```
+
+Run the following to verify generated certificates:
+```bash
+export PYTHONPATH=/verify-cert/src
+/verify-cert/ecpp-verify -i /fastecpp/certs/cert-500-500-499-460
+```
+
+## Notes
+FastECPP is relatively fast. It takes a fast PC on a single core around 35 seconds to test & verify a 1000 digit number, but over 500 seconds to process a 2000 digit number. With multiple cores times are a lot better, but should be tested for better insights. Don't expect to prove any million digit PRP soon
--- runandverify.sh
+++ runandverify.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
+# Need to set this var for verify-cert
 export PYTHONPATH=/verify-cert/src
 
+# Create filename for cert, which is 'cert-md5(input)'
 CERT_NAME=$(echo $1 | md5sum - | awk '{print $1}')
 CERT_FILENAME=cert-$CERT_NAME
 
+# Change to anything but zero to run multi-core
+# IMPORTANT: the code below does not in any way limit the amount of cores.
+# It just uses all of them, even ones you didn't know you had
+# Use at own risk!
 RUN_MPI=0
 
+# Run ecpp test
 if [ $RUN_MPI != 0 ]; then
+    # Need to explicitly set these vars to run MPI as root
+    export OMPI_ALLOW_RUN_AS_ROOT=1
+    export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
     mpirun ecpp-mpi -n "$1" -c -f /fastecpp/$CERT_FILENAME
 else
     ecpp -n "$1" -c -f /fastecpp/$CERT_FILENAME # &> /dev/null
 fi
 
+# Move the .primo certfile for certification as it's the only format the verifier can handle
 cp /fastecpp/$CERT_FILENAME.primo /verify-cert/certs/
 
+# Clean up created certs
 rm /fastecpp/$CERT_FILENAME*
 
 cd /verify-cert
 
+# Verify cert and log to either 'foundprimes.txt' or 'notfoundprimes.txt'
 if n=$(./ecpp-verify -i ./certs/$CERT_FILENAME.primo | grep -irn 'certificate is a prime number' -); then
     echo "$1 is prime (certificate: $CERT_FILENAME)" >> foundprimes.txt
     echo "$1 is prime!"
